fbacf4f9e249a8cf8f8fce686d369691bccbd16b
Erik Huang <erikhuangdrums@gmail.com>
2021-03-16 10:39:09 -0700

    commit tree a94f30565f317e2eac016881f28d480d4b3c1005

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
@@ -6,4 +7,25 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+    state = { reviews: [] };
+
+    componentDidMount = () => {
+        fetch(URL)
+            .then(res => res.json())
+            .then(data => this.setState({ reviews: data.results }))
+        // const res = await axios.get(URL);
+        // this.setState({ reviews: res.data.results });
+        // console.log(this.state.reviews)
+    }
+
+    render() {
+        return (
+            <div className="latest-movie-reviews">
+                <MovieReviews reviews={this.state.reviews} />
+            </div>
+        );
+    }
+}
+
+export default LatestMovieReviewsContainer;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
@@ -6,4 +7,34 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+    state = { reviews: [], searchTerm: '' };
+
+    onFormSubmit = e => {
+        e.preventDefault();
+        fetch(URL)
+            .then(res => res.json())
+            .then(data => this.setState({ reviews: data.results }))
+        // const res = await axios.get(URL);
+        // this.setState({ reviews: res.data.results });
+    }
+
+    render() {
+        return (
+            <div className="searchable-movie-reviews">
+                <form onSubmit={this.onFormSubmit}>
+                    <input 
+                        name="search"
+                        value={this.state.searchTerm}
+                        onChange={e => this.setState({ searchTerm: e.target.value })}
+                    />
+                </form>
+                <div>
+                    <MovieReviews reviews={this.state.reviews} />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default SearchableMovieReviewsContainer;
