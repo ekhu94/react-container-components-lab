50ab9fc034e181b57df0dce2f252af6471514849
Erik Huang <erikhuangdrums@gmail.com>
2021-03-16 10:39:13 -0700

    done

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,14 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = props => {
+    const renderedRevs = props.reviews.map(r => {
+        return <li key={r.display_title} className="review">{r.headline}</li>
+    });
+    return (
+        <ul className="review-list">
+            {renderedRevs}
+        </ul>
+    );
+}
+
+export default MovieReviews;
